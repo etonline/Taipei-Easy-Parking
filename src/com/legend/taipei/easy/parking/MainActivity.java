6344aca05f2b6561f6c4cecf15fe570a88253a6a
Jacky Lin <online.et@gmail.com>
2012-11-01 23:47:07 +0800

    Add custom ListView for display nearest parking lot

--- src/com/legend/taipei/easy/parking/MainActivity.java
+++ src/com/legend/taipei/easy/parking/MainActivity.java
@@ -1,8 +1,10 @@
 package com.legend.taipei.easy.parking;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,4 +19,11 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    /** Called when the user clicks the Find Nearest Parking Lot button */
+    public void openFindParkingLot(View view) {
+    	Intent intent = new Intent(this, DisplayNearestParkingLotActivity.class);
+    	startActivity(intent);
+    }
+    
 }
