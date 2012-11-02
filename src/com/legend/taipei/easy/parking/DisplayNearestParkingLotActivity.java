5323a21093bc0ecae389c0d458ea9fbfe6626080
Jacky Lin <online.et@gmail.com>
2012-11-02 10:48:01 +0800

    Create dummy data for testing and add function of launching Google Map using intent for direction to parking lot

--- src/com/legend/taipei/easy/parking/DisplayNearestParkingLotActivity.java
+++ src/com/legend/taipei/easy/parking/DisplayNearestParkingLotActivity.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
@@ -46,7 +51,7 @@ public class DisplayNearestParkingLotActivity extends ListActivity {
     	txtMessage = (TextView)findViewById(R.id.txtMessage);
     	txtMessage.setText("Found current GPS location, choose nearest parking lot from below:");
     	
-    	//Test data, this version is only for testing ListView using SimpleAdapter
+    	//Dummy data, this version is only for testing ListView using SimpleAdapter
     	String [] parkName = new String []{
     			"Test Parking Lot", "Test Parking Lot2"};
     	String [] parkAddress = new String []{
@@ -74,6 +79,21 @@ public class DisplayNearestParkingLotActivity extends ListActivity {
     			R.id.txtParkTotalSlot, R.id.txtParkAvailSlot});
     	setListAdapter(adapter);
     	
+    	ListView listview = getListView();
+    	listview.setOnItemClickListener(listviewOnItemCliks);
     }
+    
+    AdapterView.OnItemClickListener listviewOnItemCliks = new
+    		AdapterView.OnItemClickListener() {
+    	public void onItemClick(AdapterView<?> parent,View view,
+    			int position, long id) {
+    		
+    		//Dummy data, for testing open Google Map
+    		Intent intent = new Intent(android.content.Intent.ACTION_VIEW, 
+    		Uri.parse("http://maps.google.com/maps?saddr=25.034828,121.548872&daddr=台北市大安區信義路三段166巷6弄12號"));
+    		startActivity(intent);
+    		
+    	}
+			};
 
 }
